0b67a8add4cbe77b26dcc60e622c05466cbdba6e
Shubham-Rasal <bluequbits@gmail.com>
2025-07-05 18:53:13 +0530

    feat: update Next.js configuration for API route rewrites and headers; add new dependencies for improved functionality

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,44 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    rewrites: async () => {
+        return [
+            {
+                // Exclude internal routes and only rewrite external API routes
+                source: "/api/external/:path*",
+                destination:
+                    process.env.NODE_ENV === "development"
+                        ? "http://localhost:80/api/v1/:path*"
+                        : "/api/",
+            },
+            {
+                source: "/docs",
+                destination:
+                    process.env.NODE_ENV === "development"
+                        ? "http://localhost:80/docs"
+                        : "/api/docs",
+            },
+            {
+                source: "/openapi.json",
+                destination:
+                    process.env.NODE_ENV === "development"
+                        ? "http://localhost:80/openapi.json"
+                        : "/api/openapi.json",
+            },
+        ];
+    },
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
