184db8f1c88c220770931ff7bc249dceffe14e93
Shubham-Rasal <bluequbits@gmail.com>
2025-11-09 23:53:02 +0530

    remove config issue

--- groundline/site/next.config.ts
+++ groundline/site/next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone', // Enable standalone mode for Docker
-  nodeMiddleware: true,
 };
 
 export default nextConfig;
